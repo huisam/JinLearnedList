4b4ae61efdc778acd78bcce9d3b139a3a0459478
huisam <huisam@naver.com>
2018-06-28 06:57:56 +0000

    testing fabric

--- fabfile.py
+++ fabfile.py
@@ -1,15 +1,23 @@
 from __future__ import with_statement
 from fabric.contrib.console import confirm
 from fabric.api import local,settings,abort,run,cd,env
+from fabric.colors import red,green,blue
 from time import sleep
 env.hosts = ['localhost']
 
-
 def test():
     with settings(warn_only=True):
-        result = local('./manage.py test my_app', capture=True)
+        result = local("./manage.py test my_app", capture=True)
     if result.failed and not confirm("Tests failed. Continue anyway?"):
         abort("Aborting at user request.")
+
+def new_user(username, admin='no', comment="No comment provided"):
+    print("New User (%s): %s" % (username, comment))
+    pass
+
+def color():
+    print(green("This is green."))
+
 def commit():
     local("git add -A && git commit")
 
@@ -26,7 +34,6 @@ def hostname():
 
 def ls():
         run('ls')
-
 def deploy():
     code_dir = '/home/ubuntu/jinmine'
     with settings(warn_only=True):
@@ -36,3 +43,7 @@ def deploy():
     with cd(code_dir):
         run("git pull")
         run("cat manage.py") 
+
+def exists(path):
+    with settings(warn_only=True):
+        return run('test -e %s' % path)
