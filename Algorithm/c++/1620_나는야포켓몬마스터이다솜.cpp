646f8fff816136baf0e0f5438b1010a297235317
huisam <huisam@naver.com>
2019-10-19 21:02:15 +0900

    refactor: [HashTable] Chainning에서 Open Addressing으로 수정

--- Algorithm/c++/1620_나는야포켓몬마스터이다솜.cpp
+++ Algorithm/c++/1620_나는야포켓몬마스터이다솜.cpp
@@ -1,25 +1,48 @@
 #include<iostream>
 
 #define MAX_TABLE 200007
-#define MAX_N 100000
-#define MAX_LENGTH 21
-
-struct PoketMon {
-	char name[MAX_LENGTH];
-	int num;
-	PoketMon *prev;
-	PoketMon *next;
-};
-
-char poketMonName[MAX_N][MAX_LENGTH];
-PoketMon hashTable[MAX_TABLE];
-PoketMon pdata[MAX_N];
-char name[20];
-char ans[20];
-
-int pCnt;
+#define MAX_DATA 100001
+#define MAX_NAME 21
 
 using namespace std;
+
+void mstrcpy(char *dst, const char *src) {
+	int pos = 0;
+	while (src[pos]) {
+		dst[pos] = src[pos];
+		pos++;
+	}
+	dst[pos] = '\0';
+}
+
+int mstrcmp(const char *str1, const char *str2) {
+	int pos = 0;
+	while (str1[pos]) {
+		if (str1[pos] != str2[pos])
+			break;
+		pos++;
+	}
+	return str1[pos] - str2[pos];
+}
+
+int matoi(const char *str) {
+	int ret = 0;
+	while (*str) {
+		ret = ret * 10 + *str - '0';
+		str += 1;
+	}
+	return ret;
+}
+
+bool isNumber(const char *str) {
+	int pos = 0;
+	while (str[pos]) {
+		if (str[pos] - '0' > 9 || str[pos] - '0' < 0)
+			return false;
+		pos++;
+	}
+	return true;
+}
 
 unsigned long myhash(const char *str)
 {
@@ -34,97 +57,76 @@ unsigned long myhash(const char *str)
 	return hash % MAX_TABLE;
 }
 
-void mstrcpy(char *dst, const char *src) {
-	int pos = 0;
-	while (dst[pos] = src[pos]) {
-		pos++;
-	}
-}
 
-int mstrcmp(const char *str, const char *str2) {
-	int pos = 0;
-	while (str[pos]) {
-		if (str[pos] != str2[pos])
-			break;
-		pos++;
-	}
-	return str[pos] - str2[pos];
-}
+struct Poketmon {
+	char name[MAX_NAME];
+	int num;
+};
 
-int matoi(const char *str) {
-	int ret = 0;
-	while (*str) {
-		ret = ret * 10 + *str - '0';
-		str += 1;
-	}
-	return ret;
-}
+struct Node {
+	Poketmon *poketmon;
+};
 
-PoketMon* newNode() {
-	PoketMon *node = &pdata[pCnt++];
+
+Poketmon poketmons[MAX_DATA];
+Node hashTable[MAX_TABLE];
+int pCnt = 1;
+char name[MAX_NAME];
+
+Poketmon *newMonster(const char *name, int num) {
+	Poketmon *node = &poketmons[pCnt++];
+	mstrcpy(node->name, name);
+	node->num = num;
 	return node;
 }
 
-bool isNumber(const char *str) {
-	int pos = 0;
-	while (str[pos]) {
-		if (str[pos] - '0' > 9 || str[pos] - '0' < 0)
-			return false;
-		pos++;
+void addHashTable(Poketmon *poketmon) {
+	int key = myhash(poketmon->name);
+
+	Node *node = &hashTable[key];
+
+	while (node->poketmon != 0) {
+		key = (key + 1) % MAX_TABLE;
+		node = &hashTable[key];
 	}
-	return true;
-}
-
-void addHashTable(const int num, const char *name) {
-	PoketMon *node = newNode();
-	mstrcpy(node->name, name);
-	node->num = num;
-
-	int key = myhash(name);
-
-	PoketMon *head = hashTable[key].next;
-
-	node->next = head;
-	node->prev = &hashTable[key];
-
-	if (head != 0) head->prev = node;
-	hashTable[key].next = node;
+	node->poketmon = poketmon;
 }
 
 int findHashTable(const char *name) {
 	int key = myhash(name);
 
-	PoketMon *node = hashTable[key].next;
-	
-	while (node) {
-		PoketMon *next = node->next;
-		if (!mstrcmp(node->name, name)) {
-			return node->num;
-		}
-		node = next;
+	Node *node = &hashTable[key];
+
+	while (mstrcmp(node->poketmon->name, name) != 0) {
+		key = (key + 1) % MAX_TABLE;
+		node = &hashTable[key];
 	}
-	return -1;
+
+	return node->poketmon->num;
 }
 
 int main() {
 	ios::sync_with_stdio(false);
 	cin.tie(NULL);
+
 	int n, m;
 	cin >> n >> m;
-	for (int i = 1; i <= n; i++) {
+
+
+	for (register int i = 1; i <= n; i++) {
 		cin >> name;
-		mstrcpy(poketMonName[i], name);
-		addHashTable(i, name);
+		Poketmon *node = newMonster(name, i);
+		addHashTable(node);
 	}
 
-	for (int i = 1; i <= m; i++) {
+	while (m--) {
 		cin >> name;
 		if (isNumber(name)) {
 			int num = matoi(name);
-			cout << poketMonName[num] << '\n';
+			cout << poketmons[num].name << '\n';
 		}
-		else {
+		else
 			cout << findHashTable(name) << '\n';
-		}
 	}
+
 }
