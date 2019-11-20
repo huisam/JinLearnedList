f51d571026d07049ff6df9f3629cddb6054e99f0
huisam <huisam@naver.com>
2019-11-20 23:08:19 +0900

    refactor: heap 연산자 오버로딩방식 구현

--- DataStructure/c++/HeapString.cpp
+++ DataStructure/c++/HeapString.cpp
@@ -2,14 +2,7 @@
 
 using namespace std;
 
-void mstrcpy(char *dst, const char *src) {
-	int pos = 0;
-	while (dst[pos] = src[pos]) {
-		pos++;
-	}
-}
-
-int mstrcmp(const char *str1, const char* str2) {
+int mstrcmp(const char *str1, const char *str2) {
 	int pos = 0;
 	while (str1[pos]) {
 		if (str1[pos] != str2[pos])
@@ -19,60 +12,85 @@ int mstrcmp(const char *str1, const char* str2) {
 	return str1[pos] - str2[pos];
 }
 
-#define MAX_SIZE 100000
+void mstrcpy(char *dst, const char *src) {
+	int pos = 0;
+	while (src[pos]) {
+		dst[pos] = src[pos];
+		pos++;
+	}
+	dst[pos] = '\0';
+}
 
 struct Node {
-	char data[20];
+	char name[20];
+
+	bool operator<(const Node node) {
+		return mstrcmp(name, node.name) < 0;
+	}
+
+	bool operator<=(const Node node) {
+		return mstrcmp(name, node.name) <= 0;
+	}
 };
 
-Node heap[MAX_SIZE];
-int heapSize;
-char copys[20];
+Node pool[10000];
+int nCnt;
 
-void mswap(const int idx, const int idx2) {
-	mstrcpy(heap[MAX_SIZE - 1].data, heap[idx].data);
-	mstrcpy(heap[idx].data, heap[idx2].data);
-	mstrcpy(heap[idx2].data, heap[MAX_SIZE - 1].data);
+Node *newNode(const char *name) {
+	Node *node = &pool[nCnt++];
+	mstrcpy(node->name, name);
+	return node;
 }
 
-void addHeapNode(const char *str) {
-	mstrcpy(heap[++heapSize].data, str);
-	int i = heapSize;
+struct Heap {
+	Node *heap[100];
+	int heapSize;
 
-	while (i > 1 && mstrcmp(heap[i / 2].data, heap[i].data) > 0) {
-		mswap(i / 2, i);
-		i /= 2;
-	}
-}
-
-char *popHeapNode() {
-	mstrcpy(copys, heap[1].data);
-	mstrcpy(heap[1].data, heap[heapSize--].data);
-	int i = 1;
-
-	while (i * 2 <= heapSize) {
-		int left = i * 2;
-
-		if (mstrcmp(heap[left].data, heap[left + 1].data) > 0)
-			left++;
-
-		if (mstrcmp(heap[i].data, heap[left].data) <= 0)
-			break;
-
-		mswap(i, left);
-		i = left;
+	void mswap(int a, int b) {
+		Node *temp = heap[a];
+		heap[a] = heap[b];
+		heap[b] = temp;
 	}
 
-	return copys;
-}
+	void addHeap(Node *node) {
+		heap[++heapSize] = node;
+		int i = heapSize;
+
+		while (i > 1 && *heap[i] < *heap[i >> 1]) {
+			mswap(i, i >> 1);
+			i = i >> 1;
+		}
+	}
+
+	Node *popHeap() {
+		Node *ret = heap[1];
+		heap[1] = heap[heapSize--];
+
+		int i = 1;
+
+		while (i * 2 <= heapSize) {
+			int left = i << 1;
+			if (*heap[left + 1] < *heap[left])
+				left++;
+			if (*heap[i] <= *heap[left])
+				break;
+
+			mswap(i, left);
+			i = left;
+		}
+		return ret;
+	}
+};
+
+Heap heap;
 
 int main() {
-	addHeapNode("gogo");
-	addHeapNode("abc");
-	addHeapNode("aaa");
+	heap.addHeap(newNode("abc"));
+	heap.addHeap(newNode("ddd"));
+	heap.addHeap(newNode("zed"));
+	heap.addHeap(newNode("aa"));
 
-	popHeapNode();
-	popHeapNode();
-	popHeapNode();
-
+	while (heap.heapSize) {
+		cout << heap.popHeap()->name << '\n';
+	}
 }
